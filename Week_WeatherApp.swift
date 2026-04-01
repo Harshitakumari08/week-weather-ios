640bb5dfafd78310a32ac400859276773ea1e297
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2026-04-01 20:24:11 +0530

    Initialize Week Weather App with main structure

---
+++ Week_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week_WeatherApp.swift
+//  Week_Weather
+//
+//  Created by Harshita Jiaswal on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Week_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
