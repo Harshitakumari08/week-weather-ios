2be1d5b26a4c478a93028a0c3637adeac8a960c0
Harshita Kumari <harshita.23scse1180474@galgotiasuniversity.ac.in>
2026-04-01 20:23:35 +0530

    Add Weeks struct for weather data

---
+++ Week.swift
@@ -0,0 +1,14 @@
+//
+//  Week.swift
+//  Week_Weather
+//
+//  Created by Harshita Jiaswal on 10/12/25.
+//
+
+import Foundation
+struct Weeks {
+    var weeksDay : String = " "
+    var weather : String = " "
+    var lowTemperature : Int = 0
+    var highTemperature : Int = 0
+}
